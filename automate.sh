078ca9ea1b4f2f7689dadef3c7a5fa07f8a38e02
fvesp18 <1081@holbertonschool.com>
2020-05-01 02:09:22 +0000

    [UPDATE]

--- automate.sh
+++ automate.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-for i in [1..3]
-do
-	echo "tester" > README.md
-	git add README.md
-	git commit -m "[UPDATE]"
-	git push https://github.com/ArthurAbeshouse/AirBnB_clone_v3
-done
+git add .
+git commit -m "[UPDATE]"
+git push https://github.com/ArthurAbeshouse/AirBnB_clone_v3
